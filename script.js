c2e14520e7d99772c71731ceda08f4a19cff638e
francomendezok <125481413+francomendezok@users.noreply.github.com>
2023-05-22 12:42:17 -0300

    UI Done

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 function game () {
     var playerScore = 0;
     var cpuScore = 0;
+    var playerSelection;
+    var computerSelection;
+               // Add event listener when I select an image//
+       const options = document.querySelectorAll(".option");
+       const live = document.querySelector(".liveScore");
+       const button = document.querySelector("#playAgain");
 
-    while (playerScore <= 5 || cpuScore <= 5) {
-        // Add event listener when I select an image//
+       options.forEach(option => option.addEventListener ("click", handleClick));  
+       button.addEventListener ("click", function () {
+        playerScore = 0;
+        cpuScore = 0;
+        button.style.display = 'none';
+        live.textContent = "";
+        options.forEach(option => option.addEventListener ("click", handleClick));  
+       })      
 
-        const playerSelection = prompt("Rock, Papper or Scissors").toLowerCase();
+       function handleClick (event) {
+        playerSelection = event.target.id;
+        computerSelection = getComputerChoice();
+        result = playRound(playerSelection,computerSelection);
+        if (result.includes("Win!")) playerScore++;
+        if (result.includes("Lose!")) cpuScore++;
+        
+        live.textContent = result + "Score:" + playerScore + " - " + cpuScore;
 
-        const computerSelection = getComputerChoice()
-        let round = playRound(playerSelection, computerSelection);
-
-        if (round.includes("Win!")) playerScore++;
-        if (round.includes("Lose!")) cpuScore++;
-
-        console.log(round);
-        console.log("Score:" + playerScore + " - " + cpuScore);
         if (playerScore === 5) {
-            console.log("You Wins");
+            live.textContent = "You Wins";
+            button.style.display = 'block';
+            options.forEach(option => option.removeEventListener ("click", handleClick));  
             return;
         }
         if (cpuScore === 5) {
-            console.log("AI wins");
+            live.textContent = "You Lose";
+            button.style.display = 'block';
+            options.forEach(option => option.removeEventListener ("click", handleClick));  
             return;
         }
     }
+
+        
+    
 
 }
 
@@ -51,17 +69,17 @@ function getComputerChoice () {
 
 function playRound (playerSelection, computerSelection) {
     // PLAYER //
+    
     if (playerSelection === computerSelection) return "It's a Tie";
-    if (playerSelection === "rock" && computerSelection === "scissors") return "You Win! Rock beats Scissors"; 
-    if (playerSelection === "papper" && computerSelection === "rock") return "You Win! Papper beats Rock";  
-    if (playerSelection === "scissors" && computerSelection === "papper") return "You Win! Scissors beats Papper";
+    if (playerSelection ===  "rock" && computerSelection === "scissors") return "You Win! Rock beats Scissors"; 
+    if (playerSelection ===  "papper" && computerSelection === "rock") return "You Win! Papper beats Rock";  
+    if (playerSelection ===  "scissors" && computerSelection === "papper") return "You Win! Scissors beats Papper";
 
     // CPU //
-    if (computerSelection === "rock" && playerSelection === "scissors") return "You Lose! Rock beats Scissors";
-    if (computerSelection === "papper" && playerSelection === "rock") return "You Lose! Papper beats Rock";
-    if (computerSelection === "scissors" && playerSelection === "papper") return "You Lose! Scissors beats Papper";
+    if (computerSelection === "rock" && playerSelection ===  "scissors") return "You Lose! Rock beats Scissors";
+    if (computerSelection === "papper" && playerSelection ===  "rock") return "You Lose! Papper beats Rock";
+    if (computerSelection === "scissors" && playerSelection ===  "papper") return "You Lose! Scissors beats Papper";
 }
-
 
 game();
 
